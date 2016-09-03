47599e8a94b2665a976249fac4c87161f6cb6e1b
Joseph Gefroh <joseph.gefroh@gmail.com>
2016-09-02 16:02:26 -1000

    Made market search behave more consistently.

--- frontend/app/modules/markets/markets-map-controller.js
+++ frontend/app/modules/markets/markets-map-controller.js
@@ -7,27 +7,26 @@
   function Controller($scope, MarketsService, $timeout) {
     var vm = this;
     var timeoutTilSearch;
-    var ignoreMovement = false;
     function initialize() {
       vm.loading = {};
       vm.errors = {};
       vm.bounds = {};
+      vm.markets = [];
       vm.currentPosition = null;
       vm.daysSelected = [];
+      vm.searchMode = 'bounds';
       initializeDays();
 
       $scope.$watch('vm.bounds', function() {
         if (timeoutTilSearch) {
-          vm.loading['query-markets'] = false;
           $timeout.cancel(timeoutTilSearch);
         }
-        if (!ignoreMovement) {
-          vm.loading['query-markets'] = true;
-          timeoutTilSearch = $timeout(function() {
+        timeoutTilSearch = $timeout(function() {
+          if (vm.searchMode === 'bounds') {
             vm.findWithin(vm.bounds);
-          }, 400);
-        }
-      });
+          }
+        }, 400);
+    });
     }
 
     vm.findWithin = function(bounds) {
@@ -36,17 +35,33 @@
         ne_lng: bounds.getNorthEast().lng(),
         sw_lat: bounds.getSouthWest().lat(),
         sw_lng: bounds.getSouthWest().lng(),
-        find_within: true,
-        position_known: !!vm.currentPosition,
-        current_lat: vm.currentPosition ? vm.currentPosition.latitude : null,
-        current_lng: vm.currentPosition ? vm.currentPosition.longitude : null
+        find_within: true
       };
 
       vm.loading['query-markets'] = true;
       MarketsService.query(params).then(function(markets) {
-        vm.markets = markets;
+        if (params.position_known) {
+          vm.markets = markets;
+        }
+        else {
+          mergeMarketLists(vm.markets, markets);
+        }
       }).finally(function() {
           vm.loading['query-markets'] = false;
+      });
+    }
+
+    function mergeMarketLists(existingMarkets, newMarkets) {
+      angular.forEach(newMarkets, function(newMarket) {
+        var alreadyInList = false;
+        angular.forEach(existingMarkets, function(existingMarket) {
+          if (existingMarket.id === newMarket.id) {
+            alreadyInList = true;
+          }
+        });
+        if (!alreadyInList) {
+          existingMarkets.push(newMarket);
+        }
       });
     }
 
@@ -55,20 +70,37 @@
         vm.loading['detect-location'] = true;
         navigator.geolocation.getCurrentPosition(function(currentPosition) {
           vm.currentPosition = currentPosition.coords;
+          vm.searchMode = 'nearby';
           $scope.$broadcast('location:show', currentPosition.coords);
+          var params = {
+            position_known: true,
+            current_lat: vm.currentPosition ? vm.currentPosition.latitude : null,
+            current_lng: vm.currentPosition ? vm.currentPosition.longitude : null
+          };
+
+          MarketsService.query(params).then(function(nearbyMarkets) {
+            vm.markets = nearbyMarkets;
+          });
+
           vm.loading['detect-location'] = false;
         }, function() {
+          vm.showAll();
           vm.loading['detect-location'] = false;
         });
       }
     }
 
+    vm.showAll = function() {
+      vm.searchMode = 'bounds';
+      vm.findWithin(vm.bounds);
+    }
+
+    vm.showNearby = function() {
+      vm.detectLocation();
+    }
+
     vm.panToMarket = function(market) {
-      ignoreMovement = true;
       $scope.$broadcast('location:show', market);
-      $timeout(function() {
-        ignoreMovement = false;
-      }, 600);
     };
 
     function initializeDays() {
